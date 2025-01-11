4c50f817551b355fe4fb4386bf8345d29fe85b03
BlueAsuka <denghaoxuan1@126.com>
2025-01-11 12:33:58 +0000

    files added

--- raven.py
+++ raven.py
@@ -2,12 +2,12 @@ import os
 import json
 import pywt
 import loguru
+import pickle
 import numpy as np
 
 from data_loader import get_X_y
 from tqdm import tqdm
 from concurrent.futures import ThreadPoolExecutor
-from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import RidgeClassifierCV
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
@@ -27,26 +27,10 @@ def minmax_scale(X):
     minmax_result = 2 * (X - np.min(X, axis=1, keepdims=True)) / (np.max(X, axis=1, keepdims=True) - np.min(X, axis=1, keepdims=True)) - 1
     return minmax_result
 
-# def raven(sample, random_scales, wavelet):
-#     coefficients, _ = pywt.cwt(sample, random_scales, wavelet, method='conv')
-#     res = np.trapz(coefficients, axis=1) 
-#     return res
-
 def raven(sample, random_scales, wavelet):
-    # Precompute wavelet kernels for all scales
-    wavelet_kernels = [pywt.ContinuousWavelet(wavelet).wavefun(s)[0] for s in random_scales]
-    
-    # Compute CWT using FFT-based convolution for efficiency
-    sample_fft = np.fft.rfft(sample)
-    coefficients = []
-    for kernel in wavelet_kernels:
-        kernel_fft = np.fft.rfft(kernel, n=len(sample))
-        conv_result = np.fft.irfft(sample_fft * kernel_fft, n=len(sample))
-        coefficients.append(conv_result)
-    
-    # Compute integral using a faster method
-    res = np.trapz(coefficients, axis=1)  # Apply trapz to all coefficients at once
-    return np.array(res)
+    coefficients, _ = pywt.cwt(sample, random_scales, wavelet, method='conv')
+    res = np.trapz(coefficients, axis=1) 
+    return res
 
 def raven_parallel(X, random_scales, wavelet, max_workers=8):
     raven_results = []
@@ -65,32 +49,37 @@ if __name__ == "__main__":
               'spalling1', 'spalling2', 'spalling3', 'spalling4', 'spalling5', 'spalling6', 'spalling7', 'spalling8']
     LOADS = ['20kg', '40kg', '-40kg']
     
-    load = '20kg'
-    filenames_20kg = [os.listdir(os.path.join(INSTANCES_DIR, load, state)) for state in STATES]
-    filenames_20kg = [filename for sublist in filenames_20kg for filename in sublist]
-    filenames_20kg_t = [os.listdir(os.path.join(INFERENCE_DIR, load, state)) for state in STATES]
-    filenames_20kg_t = [filename for sublist in filenames_20kg_t for filename in sublist]
+    load = '40kg'
+    filenames = [os.listdir(os.path.join(INSTANCES_DIR, load, state)) for state in STATES]
+    filenames = [filename for sublist in filenames for filename in sublist]
+    filenames_t = [os.listdir(os.path.join(INFERENCE_DIR, load, state)) for state in STATES]
+    filenames_t = [filename for sublist in filenames_t for filename in sublist]
     
     loguru.logger.debug(f'Loading instance samples...')
-    X_20kg, y_20kg = get_X_y(INSTANCES_DIR, filenames_20kg, load='20kg')
+    X, y = get_X_y(INSTANCES_DIR, filenames, load=load)
     loguru.logger.debug(f'Loading inference samples...')
-    X_20kg_t, y_20kg_t = get_X_y(INFERENCE_DIR, filenames_20kg_t, load='20kg')
+    X_t, y_t = get_X_y(INFERENCE_DIR, filenames_t, load=load)
 
     loguru.logger.debug(f'Min-Max scaling instance samples...')
-    minmax_X_20kg = minmax_scale(X_20kg)
+    minmax_X = minmax_scale(X)
     loguru.logger.debug(f'Min-Max scaling inference samples...')
-    minmax_X_20kg_t = minmax_scale(X_20kg_t)
+    minmax_X_t = minmax_scale(X_t)
     
     loguru.logger.debug(f'Transforming instance samples...')
-    raven_results_20kg = np.array(raven_parallel(minmax_X_20kg, random_scales, wavelet))
+    raven_results = np.array(raven_parallel(minmax_X, random_scales, wavelet))
     loguru.logger.debug(f'Transforming inference samples...')
-    raven_results_20kg_t = np.array(raven_parallel(minmax_X_20kg_t, random_scales, wavelet))
+    raven_results_t = np.array(raven_parallel(minmax_X_t, random_scales, wavelet))
     
     ridge = RidgeClassifierCV()
     lda = LDA()
 
-    ridge.fit(raven_results_20kg, y_20kg)
-    lda.fit(raven_results_20kg, y_20kg)
+    ridge.fit(raven_results, y)
+    lda.fit(raven_results, y)
 
-    print(f'Raven + Ridge: {ridge.score(raven_results_20kg_t, y_20kg_t)}')
-    print(f'Raven + LDA: {lda.score(raven_results_20kg_t, y_20kg_t)}')
+    print(f'Raven + Ridge: {ridge.score(raven_results_t, y_t)}')
+    print(f'Raven + LDA: {lda.score(raven_results_t, y_t)}')
+
+    with open(f'outputs/random_scales.pkl', 'wb') as f:
+        pickle.dump(random_scales, f)
+    with open(f'outputs/raven_results_{load}.pkl', 'wb') as f:
+        pickle.dump(raven_results, f)
